c8436f0631168dfebf0dcb2af8bb2fca89ed18f4
Kronik502 <Kronikwenaite@gmail.com>
2025-08-26 06:24:50 +0200

    update in Contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -75,11 +75,11 @@ function Contact() {
     try {
  const serviceID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-const userID = import.meta.env.VITE_EMAILJS_USER_ID;
+const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
 
-      await emailjs.send(serviceID, templateID, formData, userID);
-      
+      await emailjs.send(serviceID, templateID, formData, publicKey);
+
       setStatus('success');
       setFormData({ name: '', email: '', subject: '', message: '' });
       
